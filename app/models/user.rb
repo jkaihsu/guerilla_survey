bfb1a641678e5acc8a19f3ad732d58b9b391d088
Apprentice <apprentice@devbootcamp.com>
2013-05-03 15:05:37 -0700

    Modified layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,22 @@ class User < ActiveRecord::Base
   has_many :surveys
   has_many :votes
 
-  
+  validates :username, :presence => true  
+  validates :username, :uniqueness => true
+  validates :password, :length => { :minimum => 5}
+  before_create :hash_password
+
+  include BCrypt
+
+  def self.authenticate(username, password)
+    user = User.find_by_username(username)
+    return nil unless user
+    db_password = Password.new(user.password)
+    db_password == password ? user : nil  
+  end
+
+  private
+  def hash_password
+    self.password = Password.create(self.password)
+  end 
 end
